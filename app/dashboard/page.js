134d2c1052b0cd816d86e9359b12ff7c3045bda7
Sahil-1827 <sga.opash@gmail.com>
2025-06-25 14:06:04 +0530

    change small mui tags

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,22 +1,24 @@
 "use client";
 
-import { useEffect } from 'react';
-import { useAuth } from '../../context/AuthContext';
-import ActivityLogList from '../../components/ActivityLog/ActivityLogList';
-import { CircularProgress } from '@mui/material';
+import { useEffect } from "react";
+import { useAuth } from "../../context/AuthContext";
+import ActivityLogList from "../../components/ActivityLog/ActivityLogList";
+import { CircularProgress, Container } from "@mui/material";
 
 const DashboardPage = () => {
   const { user } = useAuth();
 
   if (!user) {
-    return <CircularProgress />;
+    <Container sx={{ py: 4, textAlign: "center" }}>
+      return <CircularProgress />;
+    </Container>;
   }
 
   return (
-    <div className="dashboard-container">
+    <Container>
       <h1>Dashboard</h1>
       <ActivityLogList />
-    </div>
+    </Container>
   );
 };
 
--- app/tasks/edit/[id]/page.js
+++ app/tasks/edit/[id]/page.js
@@ -16,6 +16,7 @@ import {
   Alert,
   CircularProgress,
   Grid,
+  Chip,
 } from "@mui/material";
 import axios from "axios";
 // Remove this line:
